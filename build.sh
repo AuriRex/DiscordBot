335a10d9f22e03e25a46829b095e9d71477f1e42
AuriRex <xaurirexx@gmail.com>
2021-10-13 17:55:56 +0200

    docker build & compose changes, added lavalink <3 https://github.com/vishnubob/wait-for-it

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-dotnet publish -c Release --framework netcoreapp3.1
+dotnet build -c Release --framework netcoreapp3.1
 
 cd ./DiscordBot/
 docker build -t discord-bot-image -f Dockerfile .
 
-docker create --name discord-bot discord-bot-image
+#docker create --name discord-bot discord-bot-image
