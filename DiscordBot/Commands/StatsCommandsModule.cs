4ed1d9a4d2c94ad2c585ede3da8ebcb49fa29688
AuriRex <xaurirexx@gmail.com>
2021-10-22 13:01:31 +0200

    added build date and commit message to version cmd

--- DiscordBot/Commands/StatsCommandsModule.cs
+++ DiscordBot/Commands/StatsCommandsModule.cs
@@ -23,6 +23,8 @@ namespace DiscordBot.Commands
             //ctx.Channel.GetMessagesBeforeAsync();
         }
 
+        private static DateTime? _buildDate = null;
+
         [Command("bot-version")]
         [Aliases("version")]
         public async Task Version(CommandContext ctx)
@@ -36,16 +38,31 @@ namespace DiscordBot.Commands
             embed.AddField("Branch", $"{ThisAssembly.Git.Branch}", true);
             embed.AddField("Commit", $"{ThisAssembly.Git.Commit}", true);
 
-            /*            if(!string.IsNullOrEmpty(ThisAssembly.Git.Tag))
-                            embed.AddField("Tag", $"{ThisAssembly.Git.Tag}", true);*/
+            if (!string.IsNullOrEmpty(ThisAssembly.Git.Tag))
+                embed.AddField("Tag", $"{ThisAssembly.Git.Tag}", true);
 
-            var commitDate = DateTime.Parse(ThisAssembly.Git.CommitDate);
+            embed.AddField("Local Changes", $"{ThisAssembly.Git.IsDirty}", true);
+
+            // https://stackoverflow.com/questions/1600962/displaying-the-build-date
+            if (!_buildDate.HasValue)
+            {
+                _buildDate = DateTime.Parse(Properties.Resources.BuildDate);
+            }
+
+            var lastCommitMessage = Properties.Resources.LastCommitMessage;
+
+            lastCommitMessage = lastCommitMessage.Trim();
+
+            if (!string.IsNullOrWhiteSpace(lastCommitMessage))
+                embed.AddField("Commit Message", $"{lastCommitMessage}");
 
             embed.AddField("Full SHA Commit Hash", $"{ThisAssembly.Git.Sha}");
 
+            var commitDate = DateTime.Parse(ThisAssembly.Git.CommitDate);
+
             embed.AddField("Commit Date (UTC)", $"{commitDate.ToString("F")}");
-            
-            embed.AddField("Has Local Changes", $"{ThisAssembly.Git.IsDirty}");
+
+            embed.AddField("Build Date (UTC)", $"{_buildDate.Value.ToString("F")}");
 
             embed.WithFooter("Localized Commit Date");
             embed.WithTimestamp(commitDate);
