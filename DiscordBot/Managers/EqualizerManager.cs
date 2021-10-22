6e675e4283ab76b68b96f66382ed49a408f29c23
AuriRex <xaurirexx@gmail.com>
2021-10-22 12:02:14 +0200

    make EQSettings restoreable from last saved state + xmldocs for EQSettings

--- DiscordBot/Managers/EqualizerManager.cs
+++ DiscordBot/Managers/EqualizerManager.cs
@@ -6,8 +6,6 @@ using System.Collections.Generic;
 
 namespace DiscordBot.Managers
 {
-
-
     [AutoDI.Singleton]
     public class EqualizerManager
     {
@@ -34,8 +32,6 @@ namespace DiscordBot.Managers
             _eqSettingsForGuild.Add(guild, eqSettings);
             return eqSettings;
         }
-
-
     }
 
     public enum EQOffset
@@ -58,12 +54,23 @@ namespace DiscordBot.Managers
 
         private float[] _bandValues = new float[BAND_LENGTH];
         private readonly DiscordGuild _guild;
+        private EQSettings _LastAppliedSettings;
 
         public EQSettings(DiscordGuild guild)
         {
             _guild = guild;
         }
 
+        private EQSettings(DiscordGuild guild, EQSettings other) : this(guild)
+        {
+            SetBandsFromOther(other);
+        }
+
+        /// <summary>
+        /// Set the band at <paramref name="band"/> to <paramref name="value"/><br/><paramref name="value"/> must be between <see cref="BAND_VALUE_MIN"/> and <see cref="BAND_VALUE_MAX"/> (inclusive).
+        /// </summary>
+        /// <param name="band"></param>
+        /// <param name="value"></param>
         public void SetBand(int band, float value)
         {
             if (band < 0 || band >= BAND_LENGTH) throw new ArgumentException($"{nameof(band)} must be between 0 and {BAND_LENGTH - 1} (including)");
@@ -74,6 +81,11 @@ namespace DiscordBot.Managers
             _bandValues[band] = value;
         }
 
+        /// <summary>
+        /// Get the value of the band at <paramref name="band"/>.
+        /// </summary>
+        /// <param name="band"></param>
+        /// <returns></returns>
         public float GetBandValue(int band)
         {
             if (band < 0 || band >= BAND_LENGTH) throw new ArgumentException($"{nameof(band)} must be between 0 and {BAND_LENGTH - 1} (including)");
@@ -81,8 +93,13 @@ namespace DiscordBot.Managers
             return _bandValues[band];
         }
 
+        /// <summary>
+        /// Returns the current band settings and saves a copy to restore with <see cref="RestoreFromLastAppliedSettings"/>.
+        /// </summary>
+        /// <returns></returns>
         public LavalinkBandAdjustment[] GetBands()
         {
+            _LastAppliedSettings = new EQSettings(_guild, this);
             LavalinkBandAdjustment[] bands = new LavalinkBandAdjustment[_bandValues.Length];
 
             for (int i = 0; i < _bandValues.Length; i++)
@@ -93,6 +110,31 @@ namespace DiscordBot.Managers
             return bands;
         }
 
+        /// <summary>
+        /// Restores the last applied settings or resets it all to zero if the is none to reestore.
+        /// </summary>
+        public void RestoreFromLastAppliedSettings()
+        {
+            if (_LastAppliedSettings == null)
+            {
+                _bandValues = new float[BAND_LENGTH];
+                return;
+            }
+
+            SetBandsFromOther(_LastAppliedSettings);
+        }
+
+        /// <summary>
+        /// Sets all band values on this one to the ones from another <see cref="EQSettings"/> object.
+        /// </summary>
+        /// <param name="other"></param>
+        private void SetBandsFromOther(EQSettings other)
+        {
+            for (int i = 0; i < _bandValues.Length; i++)
+            {
+                _bandValues[i] = other._bandValues[i];
+            }
+        }
 
         /// <summary>
         /// Increase or decrease a specific band where 1 equals 0.05 units
@@ -112,6 +154,10 @@ namespace DiscordBot.Managers
             _bandValues[bandIndex] = Math.Clamp(value, BAND_VALUE_MIN, BAND_VALUE_MAX);
         }
 
+        /// <summary>
+        /// Get all bands as ints where 1 = 0.05 units.
+        /// </summary>
+        /// <returns></returns>
         public int[] GetBandsAsInts()
         {
             var bands = new int[_bandValues.Length];
@@ -122,6 +168,10 @@ namespace DiscordBot.Managers
             return bands;
         }
 
+        /// <summary>
+        /// Set all bands from ints where 1 = 0.05 units.
+        /// </summary>
+        /// <param name="bands"></param>
         public void SetBandsFromInts(int[] bands)
         {
             if (bands.Length != _bandValues.Length) throw new ArgumentException($"Must match array length of {BAND_LENGTH}");
@@ -131,12 +181,23 @@ namespace DiscordBot.Managers
             }
         }
 
+        /// <summary>
+        /// Checks if the band at <paramref name="bandIndex"/> is at <see cref="BAND_VALUE_MAX"/>.
+        /// </summary>
+        /// <param name="bandIndex"></param>
+        /// <returns></returns>
         public bool IsAtMax(int bandIndex)
         {
             if (bandIndex < 0 || bandIndex >= BAND_LENGTH) throw new ArgumentException($"{nameof(bandIndex)} must be between 0 and {BAND_LENGTH - 1} (including)");
 
             return _bandValues[bandIndex] >= BAND_VALUE_MAX;
         }
+
+        /// <summary>
+        /// Checks if the band at <paramref name="bandIndex"/> is at <see cref="BAND_VALUE_MIN"/>.
+        /// </summary>
+        /// <param name="bandIndex"></param>
+        /// <returns></returns>
         public bool IsAtMin(int bandIndex)
         {
             if (bandIndex < 0 || bandIndex >= BAND_LENGTH) throw new ArgumentException($"{nameof(bandIndex)} must be between 0 and {BAND_LENGTH - 1} (including)");
